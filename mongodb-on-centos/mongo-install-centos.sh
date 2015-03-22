4d1170639ffdf828d9951f71b600888e96f06f42
Mahesh Thiagarajan <mahthi@microsoft.com>
2015-03-22 02:14:56 -0700

    Testing MongoDB Out End to End

---
+++ mongodb-on-centos/mongo-install-centos.sh
@@ -0,0 +1,8 @@
+touch /etc/yum.repos.d/mongodb.repo
+echo "[mongodb]" >> /etc/yum.repos.d/mongodb.repo
+echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb.repo
+echo "baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64" >> /etc/yum.repos.d/mongodb.repo
+echo "gpgcheck=0" >> /etc/yum.repos.d/mongodb.repo
+echo "enabled=1" >> /etc/yum.repos.d/mongodb.repo
+sudo yum update
+sudo yum install mongo-10gen mongo-10gen-server
