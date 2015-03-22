bfb35b634f292fdfe7a9fc8510a4156d65afd8b7
Mahesh Thiagarajan <mahthi@microsoft.com>
2015-03-22 11:49:27 -0700

    MongoDB Fixes

--- mongodb-on-ubuntu/mongo-install-ubuntu.sh
+++ mongodb-on-ubuntu/mongo-install-ubuntu.sh
@@ -1,5 +1,4 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update
-sudo apt-get install mongodb-10gen
-sudo service mongodb start
+sudo apt-get -y update
+sudo apt-get install -y mongodb-10gen
