87866c49a75b51a9ad710631d7ec78805b5ceaae
Mahesh Thiagarajan <mahthi@microsoft.com>
2015-03-22 14:00:27 -0700

    Mongo DB Script Updates

--- mongodb-on-centos/mongo-install-centos.sh
+++ mongodb-on-centos/mongo-install-centos.sh
@@ -1,8 +1,9 @@
-touch /etc/yum.repos.d/mongodb.repo
+sudo chmod 777 /etc/yum.repos.d/mongodb.repo
+sudo touch /etc/yum.repos.d/mongodb.repo
 echo "[mongodb]" >> /etc/yum.repos.d/mongodb.repo
 echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb.repo
 echo "baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64" >> /etc/yum.repos.d/mongodb.repo
 echo "gpgcheck=0" >> /etc/yum.repos.d/mongodb.repo
 echo "enabled=1" >> /etc/yum.repos.d/mongodb.repo
-su yum update
-su yum install mongo-10gen mongo-10gen-server
+sudo yum update
+sudo yum install mongo-10gen mongo-10gen-server
