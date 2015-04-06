328136b5af0a96e1aa273f017970b192be77cad1
arsenv <arsenv>
2015-04-05 22:38:40 -0500

    Expand the memcached multi-vm template.

--- ubuntu-memcached-multi-vm/cache_test.php
+++ ubuntu-memcached-multi-vm/cache_test.php
@@ -6,7 +6,7 @@ print("Memcached servers: ".implode(", ",$serversArray)."<br>");
 
 $mem = new Memcached();
 
-for ($i=0 $i<sizeof($serversArray); $i++)
+for ($i=0; $i<sizeof($serversArray); $i++)
 {
 	$mem->addServer($serversArray[$i], 11211);
 }
@@ -20,6 +20,11 @@ if ($result) {
 	echo $result;
 } else {
 	echo "No matching key found. It will be added now.";
-	$mem->set("key1","This is the data in the key added on ".$date) or die("<br />ERROR: could not save to cache.");
+	$mem->set("key1","This is the data in the key added on ".$date) or die("Could not save");
 }
+
+print("<div><b>memcached stats</b><br>");
+print_r($mem->getStats());
+print("</div>");
+
 ?>
--- ubuntu-memcached-multi-vm/install_apache.sh
+++ ubuntu-memcached-multi-vm/install_apache.sh
@@ -14,15 +14,15 @@ echo "for i in range(0,c):" >> p.py
 echo "  octets[3]=str(int(octets[3])+1)" >> p.py
 echo "  s.append('.'.join(octets))" >> p.py
 echo "print(','.join(s))" >> p.py
-
+ 
 # Get comma delimited list of servers
 servers=$(python ./p.py)
  
 # Get today's date into YYYYMMDD format
 now=$(date +"%Y%m%d")
-
+ 
 echo "Installing apache $now, numberOfMemcachedInstances=$numberOfMemcachedInstances, subnetMemcachedPrefix=$subnetMemcachedPrefix, servers=$servers" >> /var/log/apache_install.log
-
+ 
 # Re-synchronize the package index files from their sources. An update should always be performed before an upgrade.
 apt-get -y update
  
@@ -32,18 +32,18 @@ apt-get -y update
 # Packages that are not already installed are not retrieved nor installed.
 # New versions of currently installed packages that cannot be upgraded without changing the install status of another package are left at their current version.
 # apt-get -y upgrade
-
+ 
 # Install apache, php5, and php5-memcached extension
 apt-get -y install apache2 php5 php5-memcached
-
+ 
 # Delete Apache index page
 rm /var/www/html/index.html
-
+ 
 # Download cache_test.php page
-wget -O /var/www/html/index.php https://gist.githubusercontent.com/arsenvlad/740ce2a058ef4b7654fc/raw/a30a9c4a7cb6a1de295682e1f9e1d18797ff5577/cache_test.php
-
+wget -O /var/www/html/index.php https://raw.githubusercontent.com/arsenvlad/azurermtemplates/memcached/ubuntu-memcached-multi-vm/cache_test.php
+ 
 # Replace the placeholder with the string of comma delimited memcached server IPs that was passed to this script as a parameter
 sed -i "s/{COMMA_DELIMITED_SERVERS_LIST}/$servers/g" /var/www/html/index.php
-
+ 
 # restart Apache
 apachectl restart
