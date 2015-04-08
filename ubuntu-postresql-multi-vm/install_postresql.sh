72ef8275e0e32417986052d632bea07d8bc01cd6
Arsen Vladimirskiy <arsenvlad@users.noreply.github.com>
2015-04-08 12:01:40 -0500

    Fix bash if statements and replicator password parameter

--- ubuntu-postresql-multi-vm/install_postresql.sh
+++ ubuntu-postresql-multi-vm/install_postresql.sh
@@ -17,7 +17,7 @@ export PGPASSWORD=$REPLICATORPASSWORD
 
 logger "NOW=$now MASTERIP=$MASTERIP SUBNETADDRESS=$SUBNETADDRESS NODETYPE=$NODETYPE NODEIP=$NODEIP NUMBEROFSLAVES=$NUMBEROFSLAVES"
 
-install_postgresql() {
+install_postgresql_service() {
 	logger "Start installing PostreSQL..."
 	# Re-synchronize the package index files from their sources. An update should always be performed before an upgrade.
 	apt-get -y update
@@ -35,7 +35,8 @@ configure_streaming_replication() {
 	logger "Starting configuring PostgreSQL streaming replication..."
 	
 	# Configure the MASTER node
-	if [ "$NODETYPE" == "MASTER" ]; then
+	if [ "$NODETYPE" == "MASTER" ];
+	then
 		logger "Create user replicator..."
 		echo "CREATE USER replicator WITH REPLICATION PASSWORD '$PGPASSWORD';"
 		sudo -u postgres psql -c "CREATE USER replicator WITH REPLICATION PASSWORD '$PGPASSWORD';"
@@ -53,11 +54,12 @@ configure_streaming_replication() {
 		echo "Already in pg_hba.conf"
 	else
 		# Allow access from other servers in the same subnet
-		echo -e "\n# install_postgresql.sh" >> pg_hba.conf
+		echo "" >> pg_hba.conf
+		echo "# install_postgresql.sh" >> pg_hba.conf
 		echo "host replication replicator $SUBNETADDRESS md5" >> pg_hba.conf
 		echo "hostssl replication replicator $SUBNETADDRESS md5" >> pg_hba.conf
-		echo -e "\n" >> pg_hba.conf 
-		
+		echo "" >> pg_hba.conf
+			
 		logger "Updated pg_hba.conf"
 		echo "Updated pg_hba.conf"
 	fi
@@ -68,7 +70,8 @@ configure_streaming_replication() {
 		echo "Already in postgresql.conf"
 	else
 		# Change configuration including both master and slave configuration settings
-		echo -e "\n# install_postgresql.sh" >> postgresql.conf
+		echo "" >> postgresql.conf
+		echo "# install_postgresql.sh" >> postgresql.conf
 		echo "listen_addresses = '*'" >> postgresql.conf
 		echo "wal_level = hot_standby" >> postgresql.conf
 		echo "max_wal_senders = 10" >> postgresql.conf
@@ -77,27 +80,29 @@ configure_streaming_replication() {
 		echo "archive_mode = on" >> postgresql.conf
 		echo "archive_command = 'cd .'" >> postgresql.conf
 		echo "hot_standby = on" >> postgresql.conf
-		echo -e "\n" >> postgresql.conf
+		echo "" >> postgresql.conf
 		
 		logger "Updated postgresql.conf"
 		echo "Updated postgresql.conf"
 	fi
 
-	if [ "$NODETYPE" == "MASTER" ]; then
+	if [ "$NODETYPE" == "MASTER" ];
+	then
 		# Start service on MASTER
 		service postgresql start
 	fi
 
 	# Synchronize the slave
-	if [ "$NODETYPE" == "SLAVE" ]; then
+	if [ "$NODETYPE" == "SLAVE" ];
+	then
 		# Remove all files from the slave data directory
 		logger "Remove all files from the slave data directory"
 		sudo -u postgres rm -rf /var/lib/postgresql/9.3/main
 
 		# Make a binary copy of the database cluster files while making sure the system is put in and out of backup mode automatically
 		logger "Make binary copy of the data directory from master"
-		sudo PGPASSWORD=$PGPASSWORD -u postgres pg_basebackup -h $MASTERIP -D /var/lib/postgresql/9.3/main -U replicator -v -P -x
-	 
+		sudo PGPASSWORD=$PGPASSWORD -u postgres pg_basebackup -h $MASTERIP -D /var/lib/postgresql/9.3/main -U replicator -x
+		 
 		# Create recovery file
 		logger "Create recovery.conf file"
 		cd /var/lib/postgresql/9.3/main/
@@ -114,5 +119,5 @@ configure_streaming_replication() {
 }
 
 # MAIN ROUTINE
-install_postgresql
+install_postgresql_service
 configure_streaming_replication
