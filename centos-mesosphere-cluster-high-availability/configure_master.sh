e7c8f990086eb871616cc25c28147ad03c2dc3dd
Alex Clemmer <clemmer.alexander@gmail.com>
2015-04-08 16:02:39 -0700

    Add CentOS-based 3-master Mesosphere deployment template
    This commmit will add a template that deploys a three-master Mesosphere
    cluster to a bunch of CentOS machines. This template includes a bunch of
    goodies that are useful to devops folks, like by-default SSH login.
    
    It also includes public IP addresses for BOTH the slave and master
    nodes. This is for quick, convenient SSH access, but arguably neither
    should be public. Instead, a production cluster should run on a private
    network and operators should VPN in, at which point they can choose to
    SSH into whatever node they want. But, that is a little heavy handed for
    our demo templates, hence, in this, everything is public.

---
+++ centos-mesosphere-cluster-high-availability/configure_master.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Add the repository
+rpm -Uvh http://repos.mesosphere.io/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
+
+echo "---                                         ---"
+echo "--- Installing Mesos and ZooKeeper packages ---"
+echo "---                                         ---"
+yum -y install mesos marathon
+yum -y install mesosphere-zookeeper
+
+echo "---                       ---"
+echo "--- Configuring ZooKeeper ---"
+echo "---                       ---"
+ID=1
+ID=`expr "$1" + "$ID"`
+echo $ID | tee /var/lib/zookeeper/myid
+
+echo | tee -a /etc/zookeeper/conf/zoo.cfg
+echo "server.1=${2}:2888:3888" | tee -a /etc/zookeeper/conf/zoo.cfg
+echo "server.2=${3}:2888:3888" | tee -a /etc/zookeeper/conf/zoo.cfg
+echo "server.3=${4}:2888:3888" | tee -a /etc/zookeeper/conf/zoo.cfg
+
+echo "---                    ---"
+echo "--- Starting ZooKeeper ---"
+echo "---                    ---"
+systemctl start zookeeper
+
+echo "---                                ---"
+echo "--- Configuring Mesos and Marathon ---"
+echo "---                                ---"
+echo "zk://${2}:2181,${3}:2181,${4}:2181/mesos" | tee /etc/mesos/zk
+echo 2 | tee /etc/mesos-master/quorum
+
+echo "---                                  ---"
+echo "--- Starting Mesos/Marathon services ---"
+echo "---                                  ---"
+# Disable mesos-slave service
+systemctl stop mesos-slave.service
+systemctl disable mesos-slave.service
+
+# Restart Mesos/Marathon services to bring them up at roughly the same time
+service mesos-master restart
+service marathon restart
