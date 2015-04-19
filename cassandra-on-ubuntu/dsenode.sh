ef9bfcf6ac07063f29f3b586ac33d266e2df349d
Trent Swanson <trentmswanson@hotmail.com>
2015-04-19 00:17:31 -0700

    udpated cluster node java versions

--- cassandra-on-ubuntu/dsenode.sh
+++ cassandra-on-ubuntu/dsenode.sh
@@ -13,13 +13,9 @@ else
   log "hostname ${HOSTNAME} added to /etchosts"
 fi
 
-#Install and configure pre-requisites for datastax cluster node
-source /etc/lsb-release
-
 #Install Java
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/webupd8team-java.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu ${DISTRIB_CODENAME} main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
-apt-get update
-echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+add-apt-repository -y ppa:webupd8team/java
+apt-get -y update 
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 apt-get -y install oracle-java7-installer
