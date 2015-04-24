693e1c567c29afde105c9b46d8a1a12fa178acc7
Bin Xia <binxi@microsoft.com>
2015-04-24 13:47:13 +0800

    fixbug

--- scrapy-on-ubuntu/scrapy-install-ubuntu.sh
+++ scrapy-on-ubuntu/scrapy-install-ubuntu.sh
@@ -1,6 +1,6 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 627220E7
 echo 'deb http://archive.scrapy.org/ubuntu scrapy main' | sudo tee /etc/apt/sources.list.d/scrapy.list
-sudo apt-get update && sudo apt-get install scrapy-0.24
+sudo apt-get update && sudo apt-get -y install scrapy-0.24
 
 
 cat > myspider.py <<EOF
