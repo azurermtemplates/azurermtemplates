cbf7ffc2edfd2abef6c51c11b74a79c07791407f
Catherine Wang <cawa@microsoft.com>
2015-04-25 17:12:00 -0700

    bug fix
    fixed Database Connection bug; refactored PowerShell script to removing
    the hard-coded variables and auto-create SAS token for current storage
    account

--- web-app-dsc-vm/ConfigureWebServer.ps1
+++ web-app-dsc-vm/ConfigureWebServer.ps1
@@ -71,15 +71,6 @@ Configuration Main
       State = "Running" 
     }
 	
-	#copy the web deploy package to the target machine if the package is uploaded to a network share, for example, \\myserver\shared\webdeploy.zip
-	#File WebDeployCopy
-     #   {
-      #      Ensure = "Present"  # You can also set Ensure to "Absent"
-      #      Type = "File" # Default is "File".
-      #      Recurse = $true # Ensure presence of subdirectories, too
-      #      SourcePath = $WebDeployPackagePath
-      #      DestinationPath = "C:\WindowsAzure\WebApplication3.zip"    
-      #  }
 
 	Script DeployWebPackage
 	{
@@ -95,10 +86,15 @@ Configuration Main
 
 		$WebClient = New-Object -TypeName System.Net.WebClient
 		$Destination= "C:\WindowsAzure\WebApplication.zip" 
-		$WebClient.DownloadFile("https://github.com/CawaMS/FileShare/releases/download/releasetag/WebApplication3.zip",$destination)
+		#$WebClient.DownloadFile("https://github.com/CawaMS/FileShare/releases/download/releasetag/WebApplication3.zip",$destination)
+        $WebClient.DownloadFile($using:WebDeployPackagePath,$destination)
         $ConnectionStringName = "DefaultConnection-Web.config Connection String"
-        $ConnectionString = "Server=tcp:"+"$DbServerName"+".database.windows.net;Database="+"$DbName"+";User ID="+"$DbUserName"+";Password="+"$DbPassword"+";Trusted_Connection=False;Encrypt=True"
-        $Argument = '-source:package="C:\WindowsAzure\WebApplication.zip"' + ' -dest:auto,ComputerName="localhost",'+"username=$UserName" +",password=$Password" + ' -setParam:name="' + "$ConnectionStringName" + '"'+',value="' + "$ConnectionString" + '" -verb:sync -allowUntrusted'
+        #$ConnectionString = "Server=tcp:"+"$DbServerName"+".database.windows.net;Database="+"$DbName"+";User ID="+"$DbUserName"+";Password="+"$DbPassword"+";Trusted_Connection=False;Encrypt=True"
+        $ConnectionString = "Server=tcp:"+ "$using:DbServerName" + ".database.windows.net,1433;Database=" + "$using:DbName" + ";User ID=" + "$using:DbUserName" + "@$using:DbServerName" + ";Password=" + "$using:DbPassword"+ ";Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+        #write-host ("The connection string is: $ConnectionString")
+        #write-Output ("The connection string is: $ConnectionString")
+        $ConnectionString | Out-File -filepath C:\WindowsAzure\outfile.txt -append -width 200
+        $Argument = '-source:package="C:\WindowsAzure\WebApplication.zip"' + ' -dest:auto,ComputerName="localhost",'+"username=$using:UserName" +",password=$using:Password" + ' -setParam:name="' + "$ConnectionStringName" + '"'+',value="' + "$ConnectionString" + '" -verb:sync -allowUntrusted'
 		$MSDeployPath = (Get-ChildItem "HKLM:\SOFTWARE\Microsoft\IIS Extensions\MSDeploy" | Select -Last 1).GetValue("InstallPath")
         Start-Process "$MSDeployPath\msdeploy.exe" $Argument -Verb runas
         
