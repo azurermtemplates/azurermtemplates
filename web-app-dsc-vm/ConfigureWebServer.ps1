0dc27f5210d227a852aaec457e931304b004044d
Catherine Wang <cawa@microsoft.com>
2015-04-25 17:14:58 -0700

    remove comments

--- web-app-dsc-vm/ConfigureWebServer.ps1
+++ web-app-dsc-vm/ConfigureWebServer.ps1
@@ -86,14 +86,9 @@ Configuration Main
 
 		$WebClient = New-Object -TypeName System.Net.WebClient
 		$Destination= "C:\WindowsAzure\WebApplication.zip" 
-		#$WebClient.DownloadFile("https://github.com/CawaMS/FileShare/releases/download/releasetag/WebApplication3.zip",$destination)
         $WebClient.DownloadFile($using:WebDeployPackagePath,$destination)
         $ConnectionStringName = "DefaultConnection-Web.config Connection String"
-        #$ConnectionString = "Server=tcp:"+"$DbServerName"+".database.windows.net;Database="+"$DbName"+";User ID="+"$DbUserName"+";Password="+"$DbPassword"+";Trusted_Connection=False;Encrypt=True"
         $ConnectionString = "Server=tcp:"+ "$using:DbServerName" + ".database.windows.net,1433;Database=" + "$using:DbName" + ";User ID=" + "$using:DbUserName" + "@$using:DbServerName" + ";Password=" + "$using:DbPassword"+ ";Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-        #write-host ("The connection string is: $ConnectionString")
-        #write-Output ("The connection string is: $ConnectionString")
-        $ConnectionString | Out-File -filepath C:\WindowsAzure\outfile.txt -append -width 200
         $Argument = '-source:package="C:\WindowsAzure\WebApplication.zip"' + ' -dest:auto,ComputerName="localhost",'+"username=$using:UserName" +",password=$using:Password" + ' -setParam:name="' + "$ConnectionStringName" + '"'+',value="' + "$ConnectionString" + '" -verb:sync -allowUntrusted'
 		$MSDeployPath = (Get-ChildItem "HKLM:\SOFTWARE\Microsoft\IIS Extensions\MSDeploy" | Select -Last 1).GetValue("InstallPath")
         Start-Process "$MSDeployPath\msdeploy.exe" $Argument -Verb runas
